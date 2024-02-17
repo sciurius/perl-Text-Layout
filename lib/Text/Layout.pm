259207a17131665dd2872653a16636b89c1975cc
Johan Vromans <jvromans@squirrel.nl>
2024-02-17 17:17:43 +0100

    Packaging changes.

--- lib/Text/Layout.pm
+++ lib/Text/Layout.pm
@@ -142,8 +142,8 @@ Same as font_face="I<FAM>".
 =item size=I<FNUM>   size=I<FNUM>pt   size=I<FNUM>%
 
 Font size in 1024ths of a point (conformance mode), or in points (e.g.
-‘12.5pt’), or a percentage (e.g. ‘200%’), or one of the relative sizes
-‘smaller’ or ‘larger’.
+'12.5pt'), or a percentage (e.g. '200%'), or one of the relative sizes
+'smaller' or 'larger'.
 
 Note that in Pango conformance mode, the actual font size is 96/72
 larger. So C<"45pt"> gives a 60pt font.
@@ -322,7 +322,7 @@ sub new {
     if ( $pkg eq __PACKAGE__ ) {
 	# For convenience (and backward compatibility)...
 	unless ( @data >= 1 && ref($data[0]) =~ /^PDF::(API2|Builder)\b/ ) {
-	    croak("Please instanciate a backend, e.g. ".__PACKAGE__."::PDFAPI2");
+	    croak("Please instantiate a backend, e.g. ".__PACKAGE__."::PDFAPI2");
 	}
 	require Text::Layout::PDFAPI2;
 	return Text::Layout::PDFAPI2->new(@data); # CUL8R
